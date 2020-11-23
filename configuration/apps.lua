706239bdaea19f302372b6bd6d7b9d32a224ad19
adrielsand <61815862+adrielsand@users.noreply.github.com>
2020-11-23 11:04:02 +0200

    brave to firefox; added xbacklight
    changed brave to firefox; added xbacklight to run on startup

--- configuration/apps.lua
+++ configuration/apps.lua
@@ -15,7 +15,7 @@ return {
     screenshot = 'flameshot full -p ~/Pictures',
     region_screenshot = 'flameshot gui -p ~/Pictures',
     delayed_screenshot = 'flameshot full -p ~/Pictures -d 5000',
-    browser = 'brave',
+    browser = 'firefox',
     editor = 'gedit', -- gui text editor
     social = 'discord',
     game = rofi_command,
@@ -27,7 +27,7 @@ return {
     'picom --config ' .. filesystem.get_configuration_dir() .. '/configuration/picom.conf',
     'nm-applet --indicator', -- wifi
     'pnmixer', -- shows an audiocontrol applet in systray when installed.
-    --'blueberry-tray', -- Bluetooth tray icon
+    'blueberry-tray', -- Bluetooth tray icon
     'numlockx on', -- enable numlock
     '/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
     'xfce4-power-manager', -- Power manager
@@ -39,5 +39,6 @@ return {
     -- Add applications that need to be killed between reloads
     -- to avoid multipled instances, inside the awspawn script
     '~/.config/awesome/configuration/awspawn' -- Spawn "dirty" apps that can linger between sessions
+    'xbacklight', -- brightness
   }
 }
